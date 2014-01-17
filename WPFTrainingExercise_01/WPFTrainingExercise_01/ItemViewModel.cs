948334eaf9a73578e77dfbabbc120e7eec953614
Mark Pearl <markpearl@gmail.com>
2014-01-17 13:14:52 +0200

    Add basic main window structure

---
+++ WPFTrainingExercise_01/WPFTrainingExercise_01/ItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WPFTrainingExercise_01
+{
+    public class ItemViewModel
+    {
+        public string  Name { get; set; }   
+        
+    }
+}
---
+++ WPFTrainingExercise_01/WPFTrainingExercise_01/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace WPFTrainingExercise_01
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<ItemViewModel> Items { get; set; }
+        public ItemViewModel SelectedItem { get; set; }
+
+        public ICommand Add { get; set; }
+        public ICommand Edit { get; set; }
+        public ICommand Delete { get; set; }
+
+        public MainViewModel()
+        {
+            PopulateItems();
+            CreateCommands();
+        }
+
+        private void CreateCommands()
+        {
+            Add = new RelayCommand(AddSomething);
+            Delete = new RelayCommand(DeleteSomething, () => SelectedItem != null);
+            Edit = new RelayCommand(EditSelected, () => SelectedItem != null);
+        }
+
+        private void PopulateItems()
+        {
+            Items = new ObservableCollection<ItemViewModel>();
+            Items.Add(new ItemViewModel() {Name = DateTime.Now.ToLongTimeString()});
+        }
+
+        private void DeleteSomething()
+        {
+            Items.Remove(SelectedItem);
+        }
+
+        private void EditSelected()
+        {
+            MessageBox.Show(SelectedItem.Name);
+        }
+
+        private void AddSomething()
+        {
+            MessageBox.Show("New Item");
+            Items.Add(new ItemViewModel() { Name = DateTime.Now.ToLongTimeString() });
+        }
+    }
+}
